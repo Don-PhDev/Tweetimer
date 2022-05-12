bdd7959de01d3821b239622cda923a6653752456
Don Forrest <forrest@philippinedev.com>
2022-05-12 22:53:56 +0800

    Add Sign Up form and user account creation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,20 @@
 class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Your account has been created successfully"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
