b9a22f96ecaaa326bcc0029a33c3df94e3baf896
Don Forrest <forrest@philippinedev.com>
2022-05-13 01:22:31 +0800

    Correct words

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: "You have been successfully logged out"
+      redirect_to root_path, notice: "You have been successfully logged in"
     else
       flash[:alert] = "Invalid email or password"
       render :new
