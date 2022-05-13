62bfd3694d56ece755c8728c35bb7604ce9467f2
Don Forrest <forrest@philippinedev.com>
2022-05-13 17:33:28 +0800

    Passwords controller: edit and update methods, with routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
+  get "edit_password", to: "passwords#edit", as: :edit_password
+  patch "edit_password", to: "passwords#update"
+
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
 
